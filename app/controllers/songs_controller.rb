96ba2604679472f71828b095ab75e537c4032274
pete3249 <katelynrpeterson16@gmail.com>
2020-10-09 21:26:30 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -28,9 +28,7 @@ class SongsController < ApplicationController
   def update
     @song = Song.find(params[:id])
 
-    @song.update(song_params)
-
-    if @song.save
+    if @song.update(song_params)
       redirect_to @song
     else
       render :edit
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,22 @@
 class Song < ActiveRecord::Base
-  # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  #validations
+  validates :artist_id, :genre_id, presence: true
+  validates :title, {presence: true, uniqueness: { scope: [:artist_id]}}
+
+  def artist_name=(artist_name)
+    # artist= method 
+    # assigns the associate object, extracts the primary key, and sets it as the foreign key
+    self.artist = Artist.find_or_create_by(name: artist_name)
+    # find_or_create_by method
+    # finds the first record with the given attributes, or creates a record with the attributes if one is not found
+  end 
+
+  def artist_name
+    self.artist.try(:name)
+  end 
+                 
 end
