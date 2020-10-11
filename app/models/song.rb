eff9fb2eb4f257dc659c283103c39fe8781049c5
pete3249 <katelynrpeterson16@gmail.com>
2020-10-11 17:17:50 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,9 +3,9 @@ class Song < ActiveRecord::Base
   belongs_to :genre
   has_many :notes
 
-  #validations
-  validates :artist_id, :genre_id, presence: true
-  validates :title, {presence: true, uniqueness: { scope: [:artist_id]}}
+  # validations
+  # validates :artist_id, :genre_id, presence: true
+  # validates :title, {presence: true, uniqueness: { scope: [:artist_id]}}
 
   def artist_name=(artist_name)
     # artist= method 
@@ -17,6 +17,37 @@ class Song < ActiveRecord::Base
 
   def artist_name
     self.artist.try(:name)
+    # way of preventing no method error
+    # does object know how to respond to method
+    # if it does, call it (self.send)
+    # otherwise, return nil
+    # useful in method chaining, if case where self.artist is nil
   end 
-                 
+
+  def note_1=(note)
+    @note_1 = self.notes.build(content: note) 
+  end
+
+  def note_1
+    self.notes.first.try(:content)
+  end 
+
+  def note_2=(note)
+    @note_2 = self.notes.build(content: note) 
+  end
+
+  def note_2
+    self.notes.last.try(:content)
+  end 
+
+  def note_contents=(notes_content)
+    notes_content.each do |note_content|
+      self.notes.build(content: note_content)
+    end 
+  end  
+  
+  def note_contents
+
+  end 
+
 end
