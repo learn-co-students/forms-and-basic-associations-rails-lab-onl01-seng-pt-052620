83dfb46a042b018247a9ac2a5e8b449f77ff55a4
rumikoacopa <rumikorios@gmail.com>
2020-10-12 01:06:07 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,34 @@
 class Song < ActiveRecord::Base
-  # add associations here
+  belongs_to :artist
+  belongs_to :genre 
+  has_many :notes
+
+  #validates :artist_id, :genre_id, {presence: true}
+  #validates :title, {presence: true, uniqueness: {scope: [:artist_id]}}
+
+  def artist_name=(artist_name)
+    self.artist = Artist.find_or_create_by(name: artist_name)
+  end
+
+  def artist_name
+    self.artist.try(:name)
+  end
+
+  def notes_1 
+    self.notes.first.try(:content)
+  end
+
+
+  def notes_1=(content) 
+    @note_1 = self.notes.build(content: content)
+  end
+
+  def notes_2 
+    self.notes.last.try(:content)
+  end 
+
+  def notes_2=(content) 
+    @note_2 = self.notes.build(content: content)
+
+  end
 end
